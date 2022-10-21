d3155253565c0d108d4befb6c2a764f8d91f2b1f
hanchen <83146077+ghlsp@users.noreply.github.com>
2022-10-21 19:15:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-const Waline = require('@waline/vercel'); 
+const Waline.init = require('@waline/vercel'); 
   
- module.exports = Waline({ 
-   
-
-   reaction: [
+ Waline.init({
+  el: '#waline',
+  // ...
+  reaction: [
     'https://unpkg.com/@waline/emojis@1.1.0/weibo/weibo_heart_eyes.png',
     'https://unpkg.com/@waline/emojis@1.1.0/weibo/weibo_dog_joy.png',
     'https://unpkg.com/@waline/emojis@1.1.0/weibo/weibo_dog_consider.png',
     'https://unpkg.com/@waline/emojis@1.1.0/weibo/weibo_sob.png',
   ],
-
-   forbiddenWords: [ 
-     '习近平', 
-     '毛泽东', 
-     '快递', 
-     '空包', 
-     '代发', 
-     '单号', 
-     '购买', 
-     '傻逼', 
-     'SB', 
-     '你没🐎', 
-     '你没有妈', 
-     '你没马', 
-     '你没有🐎' 
-   ],
- });
+});
