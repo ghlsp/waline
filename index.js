b66db923663a97c5b28bfd8ca3460bd38ac6993c
hanchen <83146077+ghlsp@users.noreply.github.com>
2022-11-06 18:10:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const Application = require('@waline/vercel'); 
+const Waline = require('@waline/vercel'); 
   
- module.exports = Application({ 
-   async postSave(comment) { 
-     // do what ever you want after save comment 
-   }, 
- });
+ module.exports = Waline({ 
+   forbiddenWords: [ 
+     '习近平', 
+     '毛泽东', 
+     '快递', 
+     '空包', 
+     '代发', 
+     '单号', 
+     '购买', 
+     '傻逼', 
+     'SB', 
+     '你没🐎', 
+     '你没有妈', 
+     '你没马', 
+     '你没有🐎' 
+   ],
